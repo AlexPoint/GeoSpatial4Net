614fab7780a9a4fb4457dbd2a7ab5fd7e860a666
Alex <pointalexandre@gmail.com>
2021-03-07 22:34:25 +0100

    Add new method to compute equirectangular projection distance

--- GeoSpatial4Net.Core/GeoDistanceCalculator.cs
+++ GeoSpatial4Net.Core/GeoDistanceCalculator.cs
@@ -33,6 +33,21 @@ namespace GeoSpatial4Net
             return earthRadiusInMeters * h2;
         }
 
+        public double EquirectangularProjectionDistance(Coordinate position1, Coordinate position2)
+        {
+            var p1 = ToRadians(position2.Longitude - position1.Longitude) * Math.Cos(0.5 * ToRadians(position2.Latitude + position1.Latitude)); //convert lat/lon to radians
+            var p2 = ToRadians(position2.Latitude - position1.Latitude);
+            return earthRadiusInMeters * Math.Sqrt(p1 * p1 + p2 * p2);
+        }
+
+        public double EquirectangularProjectionDistance(double latitude1, double longitude1, double latitude2, double longitude2)
+        {
+            var coord1 = new Coordinate(latitude1, longitude1);
+            var coord2 = new Coordinate(latitude2, longitude2);
+
+            return EquirectangularProjectionDistance(coord1, coord2);
+        }
+
         private static double ToRadians(double val)
         {
             return (Math.PI / 180) * val;
--- GeoSpatial4Net.Tests/DistanceComputationTest.cs
+++ GeoSpatial4Net.Tests/DistanceComputationTest.cs
@@ -34,8 +34,10 @@ namespace GeoSpatial4Net.Tests
             Assert.AreEqual(0, Math.Floor(distCalc.HaversineDistance(coord1, coord1)));
             Assert.AreEqual(0, Math.Floor(distCalc.HaversineDistance(coord2, coord2)));
             Assert.AreEqual(0, Math.Floor(distCalc.HaversineDistance(1.25489, 65.698712, 1.25489, 65.698712)));
-            
-            Assert.AreNotEqual(0, Math.Floor(distCalc.HaversineDistance(1.25489, 65.698712, 1.25489, 66.698712)));
+
+
+            Assert.AreEqual(0, Math.Floor(distCalc.EquirectangularProjectionDistance(coord1, coord1)));
+            Assert.AreEqual(0, Math.Floor(distCalc.EquirectangularProjectionDistance(coord2, coord2)));
         }
 
         [Test]
@@ -54,6 +56,36 @@ namespace GeoSpatial4Net.Tests
             // latitude 396.12 is greater than 90; it should be -36.12 instead 
             var computedDistance2 = distCalc.HaversineDistance(396.12, -86.67, 33.94, -118.4);
             Assert.AreEqual(Math.Floor(expectedDistanceM), Math.Floor(computedDistance2));
+        }
+
+
+        [Test]
+        public void EquirectangularProjectionDistanceTest()
+        {
+            // The distance calculated with the equirectangular projection is slightly different from the Haversine distance
+            var coord1 = new Coordinate(36.12, -86.67);
+            var coord2 = new Coordinate(33.94, -118.4);
+            var distCalc = new GeoDistanceCalculator();
+            var expectedDistanceM = 2900055.0;
+            var computedDistance = distCalc.EquirectangularProjectionDistance(coord1, coord2);
+
+            Assert.AreEqual(Math.Floor(expectedDistanceM), Math.Floor(computedDistance));
+            Assert.AreEqual(expectedDistanceM, Math.Floor(distCalc.EquirectangularProjectionDistance(36.12, -86.67, 33.94, -118.4)));
+            
+            Assert.AreEqual(0, Math.Floor(distCalc.EquirectangularProjectionDistance(1.25489, 65.698712, 1.25489, 65.698712)));
+        }
+
+        [Test]
+        public void EquirectangularProjectionVsHaversineDistanceTest()
+        {
+            // The error between the two methods should be minimal for coordinate relatively close
+            var coord1 = new Coordinate(36.12, -86.67);
+            var coord2 = new Coordinate(36.94, -86.4);
+            var distCalc = new GeoDistanceCalculator();
+            var expectedDistanceM = distCalc.HaversineDistance(coord1, coord2);
+            var computedDistance = distCalc.EquirectangularProjectionDistance(coord1, coord2);
+
+            Assert.AreEqual(Math.Floor(expectedDistanceM), Math.Floor(computedDistance));
         }
     }
 }
